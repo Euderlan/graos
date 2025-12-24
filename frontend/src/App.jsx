028fc456db3295b269bd33053f891148799f242b
euderlan <euderlanfreire@gmail.com>
2025-12-23 22:54:37 -0300

    Estrutura inicial do projeto Grãos com frontend em React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Sistema de Monitoramento de Grãos</h1>
+      <p>Temperatura de São luis ta maravilhosa, logo dos grões também :)</p>
+    </div>
+  )
+}
+
+export default App
